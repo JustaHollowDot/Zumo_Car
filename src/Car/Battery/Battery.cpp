20ae53fe5099756b7be10113d9cbce5bff818abc
JustaHollowDot <emilhall@stud.ntnu.no>
2023-11-02 09:26:42 +0100

    Added battery as a component to the car

---
+++ src/Car/Battery/Battery.cpp
@@ -0,0 +1,3 @@
+//
+// Created by emil on 02.11.2023.
+//
---
+++ src/Car/Battery/Battery.h
@@ -0,0 +1,8 @@
+//
+// Created by emil on 02.11.2023.
+//
+
+#ifndef ZUMO_ROBOT_BATTERY_H
+#define ZUMO_ROBOT_BATTERY_H
+
+#endif //ZUMO_ROBOT_BATTERY_H
